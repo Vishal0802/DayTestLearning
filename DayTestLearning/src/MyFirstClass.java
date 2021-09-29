63c909079780390555e87fec647503027df889e5
Vishal0802 <vishal.confi@gmail.com>
2021-09-29 19:51:08 +0530

    Second Commit

---
+++ DayTestLearning/src/MyFirstClass.java
@@ -0,0 +1,15 @@
+
+public class MyFirstClass {
+
+	public MyFirstClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("My First Commint");
+		// TODO Auto-generated method stub
+
+	}
+
+}
